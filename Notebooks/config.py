b5f61d884de37e00f15eeca37ac84107597d6b58
Akhitha Babu <akhithababumeloor999@gmail.com>
2020-11-12 11:17:50 +0530

    Notebooks
    Jupyter Notebooks

---
+++ Notebooks/config.py
@@ -0,0 +1,7 @@
+TRAIN='../Data/rossmann-store-sales-dataset/train.csv'
+STORE='../Data/rossmann-store-sales-dataset/store.csv'
+
+MISSING_VALUE_HANDLED='../Data/missing_value_handled.csv'
+FEATURE_ENGINEERING='../Data/feature_engineering.csv'
+
+
